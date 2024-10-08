0d5584db327dad940a8565adb77b4cc9ca1fc32c
Dalila <dalila.fariello@gmail.com>
2024-10-08 12:46:47 +0200

    Refactor: getMonth function to properly return month names based on date

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -7,7 +7,7 @@ import "./style.scss";
 const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
-  
+
   // Ensure data is available before sorting
  const byDateDesc = data?.focus?.length
  ? data.focus.sort((evtA, evtB) => new Date(evtA.date) < new Date(evtB.date) ? -1 : 1)
@@ -38,36 +38,37 @@ if (byDateDesc.length === 0) {
 
   return (
     <div className="SlideCardList">
-      {byDateDesc?.map((event, idx) => (
-        <>
-          <div
-            key={event.title}
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
-            }`}
-          >
-            <img src={event.cover} alt="forum" />
-            <div className="SlideCard__descriptionContainer">
-              <div className="SlideCard__description">
-                <h3>{event.title}</h3>
-                <p>{event.description}</p>
-                <div>{getMonth(new Date(event.date))}</div>
+      {byDateDesc
+        ?.sort((a, b) => new Date(a.date) - new Date(b.date)) // Sort by date in ascending order
+        .map((event, idx) => (
+          <div key={event.title}>
+            <div
+              className={`SlideCard SlideCard--${
+                index === idx ? "display" : "hide"
+              }`}
+            >
+              <img src={event.cover} alt="forum" />
+              <div className="SlideCard__descriptionContainer">
+                <div className="SlideCard__description">
+                  <h3>{event.title}</h3>
+                  <p>{event.description}</p>
+                  <div className="month__description">{getMonth(new Date(event.date))}</div>
+                </div>
+              </div>
+            </div>
+            <div className="SlideCard__paginationContainer">
+              <div className="SlideCard__pagination">
+                {byDateDesc.map((_, radioIdx) => (
+                  <input
+                    key={event.id}
+                    type="radio"
+                    name="radio-button"
+                    checked={idx === radioIdx}
+                  />
+                ))}
               </div>
             </div>
           </div>
-          <div className="SlideCard__paginationContainer">
-            <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
-                <input
-                  key={`${event.id}`}
-                  type="radio"
-                  name="radio-button"
-                  checked={idx === radioIdx}
-                />
-              ))}
-            </div>
-          </div>
-        </>
       ))}
     </div>
   );
